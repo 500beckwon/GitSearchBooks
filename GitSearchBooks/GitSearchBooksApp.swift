5a9959ae46abe46fcf292ba6e155fe3f41faca19
500beckwon <kjsx60@gmail.com>
2024-06-10 20:23:06 +0900

    init project

---
+++ GitSearchBooks/GitSearchBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSearchBooksApp.swift
+//  GitSearchBooks
+//
+//  Created by ByungHoon Ann on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSearchBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
