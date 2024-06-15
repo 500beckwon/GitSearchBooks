168ff504e095f5b9bbddce5e6d97a5457d842d1b
500beckwon <kjsx60@gmail.com>
2024-06-15 15:44:55 +0900

    기본 정보 객체 제작

---
+++ GitSearchBooks/Common/Define.swift
@@ -0,0 +1,15 @@
+//
+//  Define.swift
+//  GitSearchBooks
+//
+//  Created by ByungHoon Ann on 6/10/24.
+//
+
+import Foundation
+
+struct Define {
+    static let baseURL = "https://api.itbook.store/1.0"
+    static let searchURL = baseURL + "search"
+    static let detailURL = baseURL + "books"
+    static let bookImageURL = "https://itbook.store/img/books"
+}
